c8899d8870536b6a1d7790c33815897f67e47c42
barahouei <abdulghanibarahouei@gmail.com>
2023-01-05 16:57:28 +0330

    Solved Lasagna Master exercise

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -1,9 +1,59 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+// PreparationTime returns the time that takes to prepare all layers.
+func PreparationTime(layers []string, timePerLayer int) int {
+	if timePerLayer == 0 {
+		timePerLayer = 2
+	}
 
-// TODO: define the 'Quantities()' function
+	layersNum := len(layers)
 
-// TODO: define the 'AddSecretIngredient()' function
+	return layersNum * timePerLayer
+}
 
-// TODO: define the 'ScaleRecipe()' function
+// Quantities returns the exact amount of noodle and sauce that needs.
+func Quantities(layers []string) (int, float64) {
+	noodlePerLayer := 50
+	saucePerLayer := 0.2
+
+	var noodleLayersNum int
+	var sauceLayersNum float64
+
+	for i := 0; i < len(layers); i++ {
+		if layers[i] == "noodles" {
+			noodleLayersNum++
+		}
+
+		if layers[i] == "sauce" {
+			sauceLayersNum++
+		}
+	}
+
+	noodle := noodleLayersNum * noodlePerLayer
+	sauce := sauceLayersNum * saucePerLayer
+
+	return noodle, sauce
+}
+
+// AddSecretIngredient sets the last element of  the friend list as the last element of my list.
+func AddSecretIngredient(friendList, myList []string) {
+	secretIngredient := friendList[len(friendList)-1]
+
+	myList[len(myList)-1] = secretIngredient
+}
+
+// ScaleRecipe returns the exact amount for different numbers of portions as a new slice.
+func ScaleRecipe(quantities []float64, portionsNum int) []float64 {
+	var quantitiesPerOne []float64
+	var totalQuantities []float64
+
+	for i := 0; i < len(quantities); i++ {
+		quantitiesPerOne = append(quantitiesPerOne, quantities[i]/float64(2))
+	}
+
+	for i := 0; i < len(quantitiesPerOne); i++ {
+		totalQuantities = append(totalQuantities, quantitiesPerOne[i]*float64(portionsNum))
+	}
+
+	return totalQuantities
+}
