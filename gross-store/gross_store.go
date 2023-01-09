3f1a60c00d6e93736511eee2bb180dc53fd00d8c
barahouei <abdulghanibarahouei@gmail.com>
2023-01-10 01:15:00 +0330

    Solved Gross Store exercise

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -2,25 +2,78 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+	units := make(map[string]int)
+
+	units["quarter_of_a_dozen"] = 3
+	units["half_of_a_dozen"] = 6
+	units["dozen"] = 12
+	units["small_gross"] = 120
+	units["gross"] = 144
+	units["great_gross"] = 1728
+
+	return units
 }
 
 // NewBill creates a new bill.
 func NewBill() map[string]int {
-	panic("Please implement the NewBill() function")
+	return make(map[string]int)
 }
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the AddItem() function")
+	_, itemExists := bill[item]
+	_, unitExists := units[unit]
+
+	if !unitExists {
+		return false
+	}
+
+	if !itemExists {
+		bill[item] = units[unit]
+
+		return true
+	} else {
+		bill[item] += units[unit]
+
+		return true
+	}
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+	_, itemExists := bill[item]
+	_, unitExists := units[unit]
+
+	if !itemExists {
+		return false
+	}
+
+	if !unitExists {
+		return false
+	}
+
+	tmp := bill[item] - units[unit]
+
+	if tmp < 0 {
+		return false
+	} else if tmp == 0 {
+		delete(bill, item)
+
+		return true
+	} else {
+		bill[item] -= units[unit]
+
+		return true
+	}
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+	_, itemExists := bill[item]
+
+	if !itemExists {
+		return 0, false
+	}
+
+	return bill[item], true
 }
