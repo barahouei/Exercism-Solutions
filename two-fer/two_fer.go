815980b3882352f9da93dfa81993fd47771552ae
barahouei <abdulghanibarahouei@gmail.com>
2023-01-21 01:12:46 +0330

    Solved Two Fer Exercise

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,15 +1,13 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer provides a way to share items between individuals.
 package twofer
 
-// ShareWith should have a comment documenting it.
+import "fmt"
+
+// ShareWith returns a string that shows one item is for me and one for another.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if name == "" {
+		return "One for you, one for me."
+	}
+
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
