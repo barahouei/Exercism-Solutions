c6701db868b9ccef3f8947e8a6d9e5eff1667a85
barahouei <abdulghanibarahouei@gmail.com>
2023-01-21 23:08:05 +0330

    Solved Raindrops Exercise

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,33 @@
+// Package raindrops create a raindrop sound.
 package raindrops
 
+import "strconv"
+
+// approvedRaindrops initializes the sounds.
+func approvedRaindrops() map[int]string {
+	sounds := make(map[int]string)
+
+	sounds[3] = "Pling"
+	sounds[5] = "Plang"
+	sounds[7] = "Plong"
+
+	return sounds
+}
+
+// Convert converts a number to a raindrop sound.
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	sounds := approvedRaindrops()
+	sound := ""
+
+	for i := 3; i <= 7; i++ {
+		if number%i == 0 {
+			sound += sounds[i]
+		}
+	}
+
+	if sound == "" {
+		sound = strconv.Itoa(number)
+	}
+
+	return sound
 }
