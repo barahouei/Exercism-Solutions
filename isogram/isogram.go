b4755e08f3dadff2cb536f42dc517738939f81fe
barahouei <abdulghanibarahouei@gmail.com>
2023-01-27 15:18:14 +0330

    Solved Isogram Exercise

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,5 +1,27 @@
+// Package isogram checks for isogram.
 package isogram
 
+import (
+	"unicode"
+)
+
+// IsIsogram checks if a word or phrase is an isogram.
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	iso := make(map[rune]int)
+
+	for _, v := range word {
+		v = unicode.ToLower(v)
+
+		if unicode.IsSpace(v) || v == '-' {
+			continue
+		}
+
+		if _, ok := iso[v]; ok {
+			return false
+		}
+
+		iso[v] += 1
+	}
+
+	return true
 }
