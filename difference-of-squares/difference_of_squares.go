cbda2ffb383da4a045e3eebb41bd53d6f90f92f5
barahouei <abdulghanibarahouei@gmail.com>
2023-01-27 18:15:49 +0330

    Solved Difference of Squares Exercise

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,13 +1,20 @@
+// Package diffsquares deals with square of sum and sum of squares.
 package diffsquares
 
+// SquareOfSum returns the square of the sum of the first n natural numbers.
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	sum := (n * (n + 1)) / 2
+	sum *= sum
+
+	return sum
 }
 
+// SumOfSquares returns the sum of the squares of the first n natural number.
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	return (n * ((n + 1) * (n*2 + 1))) / 6
 }
 
+// Difference returns the difference between square of sum and sum of squares of the first n number.
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
