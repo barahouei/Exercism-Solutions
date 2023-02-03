41dceb8a62fe9ccc50548ab68f5656aa44ba88fe
barahouei <abdulghanibarahouei@gmail.com>
2023-02-03 17:59:54 +0330

    Solved Luhn Exercise

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,5 +1,47 @@
+// Package luhn validates credit cards.
 package luhn
 
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// Valid checks if a credit card number is correct.
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	nums := []int{}
+	sum := 0
+
+	chars := []rune(strings.ReplaceAll(id, " ", ""))
+
+	if len(chars) <= 1 {
+		return false
+	}
+
+	for i := len(chars) - 1; i >= 0; i-- {
+		if !unicode.IsNumber(chars[i]) {
+			return false
+		}
+
+		number, _ := strconv.Atoi(string(chars[i]))
+
+		if (len(chars)-i)%2 == 0 {
+			number *= 2
+
+			if number > 9 {
+				number -= 9
+			}
+
+			nums = append(nums, number)
+		} else {
+
+			nums = append(nums, number)
+		}
+	}
+
+	for _, v := range nums {
+		sum += v
+	}
+
+	return sum%10 == 0
 }
