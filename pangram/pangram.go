3cc0ca180249517533a75883ca79bcd301369138
barahouei <abdulghanibarahouei@gmail.com>
2023-02-11 15:12:48 +0330

    Solved Pangram Exercise

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,19 @@
+// Package Pangram checks if a given string contains all English alphabet.
 package pangram
 
+import (
+	"strings"
+)
+
+// IsPangram checks if a string is pangram.
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	input = strings.ToLower(input)
+
+	for i := 'a'; i <= 'z'; i++ {
+		if !strings.ContainsRune(input, i) {
+			return false
+		}
+	}
+
+	return true
 }
