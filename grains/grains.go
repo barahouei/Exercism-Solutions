28710f034b65c36b812e9e9572887ab3684b283e
barahouei <abdulghanibarahouei@gmail.com>
2023-02-15 03:23:00 +0330

    Solved Grains Exercise

--- grains/grains.go
+++ grains/grains.go
@@ -1,9 +1,33 @@
+// Package grains show the number of grains in chessboard squares.
 package grains
 
+import (
+	"errors"
+)
+
+// Square returns the number of grains in a given chessboard square,
+// and an error if it accrues.
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	var grains uint64 = 1
+
+	if number < 1 || number > 64 {
+		return 0, errors.New("wrong number")
+	}
+
+	for i := 1; i < number; i++ {
+		grains *= 2
+	}
+
+	return grains, nil
 }
 
+// Total returns the total of grains in chessboard.
 func Total() uint64 {
-	panic("Please implement the Total function")
+	var grains uint64 = 1
+
+	for i := 1; i < 64; i++ {
+		grains *= 2
+	}
+
+	return grains<<grains - 1
 }
