38d09785bb34e1f103dbb091f3166382a5af7d64
barahouei <abdulghanibarahouei@gmail.com>
2023-03-02 02:42:01 +0330

    Solved Atbash Cipher Exercise

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -1,5 +1,33 @@
 package atbash
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	s = strings.ToLower(s)
+
+	cipherText := []rune{}
+	counter := 0
+
+	for _, v := range s {
+		if counter == 5 && v != '.' && v != ',' {
+			cipherText = append(cipherText, ' ')
+			counter = 0
+		}
+
+		if v >= 'a' && v <= 'z' {
+			diff := v - 'a'
+			cipherText = append(cipherText, 'z'-diff)
+
+			counter++
+		} else if unicode.IsNumber(v) {
+			cipherText = append(cipherText, v)
+
+			counter++
+		}
+	}
+
+	return string(cipherText)
 }
