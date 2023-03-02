e269409570b5f08ee7f7bf0a49a89ad16a908aac
barahouei <abdulghanibarahouei@gmail.com>
2023-03-02 04:56:00 +0330

    Solved Rotational Cipher Exercise

--- rotational-cipher/rotational_cipher.go
+++ rotational-cipher/rotational_cipher.go
@@ -1,5 +1,44 @@
+// Package rotationalcipher encodes string based on Caesar cipher.
 package rotationalcipher
 
+import "unicode"
+
+// RotationalCipher will move every character of the given string x times on the alphabet.
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	if shiftKey == 0 || shiftKey == 26 {
+		return plain
+	}
+
+	shift := rune(shiftKey)
+	cipheredText := []rune{}
+
+	for _, v := range plain {
+		if unicode.IsSpace(v) || unicode.IsNumber(v) {
+			cipheredText = append(cipheredText, v)
+		} else if v >= 'a' && v <= 'z' {
+			if v+shift > 'z' {
+				v = (v + shift) - 'z' - 1 + 'a'
+
+				cipheredText = append(cipheredText, v)
+			} else {
+				v += shift
+
+				cipheredText = append(cipheredText, v)
+			}
+		} else if v >= 'A' && v <= 'Z' {
+			if v+shift > 'Z' {
+				v = (v + shift) - 'Z' - 1 + 'A'
+
+				cipheredText = append(cipheredText, v)
+			} else {
+				v += shift
+
+				cipheredText = append(cipheredText, v)
+			}
+		} else {
+			cipheredText = append(cipheredText, v)
+		}
+	}
+
+	return string(cipheredText)
 }
