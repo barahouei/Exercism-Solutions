9d9faac376572fd8e61c74f1e13a7c86c888eec0
barahouei <abdulghanibarahouei@gmail.com>
2023-03-05 20:52:00 +0330

    Solved Reverse String Exercise

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,5 +1,14 @@
+// Package reverse reverses a string.
 package reverse
 
+// Reverse returns the reverse of a string.
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	inputRune := []rune(input)
+	result := []rune{}
+
+	for i := len(inputRune) - 1; i >= 0; i-- {
+		result = append(result, inputRune[i])
+	}
+
+	return string(result)
 }
