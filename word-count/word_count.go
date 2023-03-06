eff1e5192e139bdb42020ef28ad203d9366a8100
barahouei <abdulghanibarahouei@gmail.com>
2023-03-06 19:24:30 +0330

    Solved Word Count Exercise

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,7 +1,35 @@
+// Package wordcount counts the words in a phrase.
 package wordcount
+
+import (
+	"regexp"
+	"strings"
+)
 
 type Frequency map[string]int
 
+// WordCount returns the frequency of words in a phrase.
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	phrase = strings.ToLower(phrase)
+	wordsFrequency := make(Frequency)
+
+	words := strings.Split(phrase, " ")
+
+	for _, word := range words {
+		if regexp.MustCompile(`([a-z]+[']+[a-z])`).FindString(word) != "" {
+			wordsFrequency[word]++
+		} else {
+			word = regexp.MustCompile(`([^a-z0-9+])`).ReplaceAllString(word, " ")
+
+			s := strings.Split(word, " ")
+
+			for _, v := range s {
+				if v != "" {
+					wordsFrequency[v]++
+				}
+			}
+		}
+	}
+
+	return wordsFrequency
 }
