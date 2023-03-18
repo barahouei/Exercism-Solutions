d21d8a822be16be1c37f1ccb66199869752318f2
barahouei <abdulghanibarahouei@gmail.com>
2023-03-18 22:37:35 +0330

    Solved Roman Numerals Exercise

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -1,5 +1,46 @@
+// Package romannumerals converts int to roman numeral.
 package romannumerals
 
+import "errors"
+
+// ToRomanNumeral converts an integer to it's equivelent in Roman.
 func ToRomanNumeral(input int) (string, error) {
-	panic("Please implement the ToRomanNumeral function")
+	if input > 3999 || input <= 0 {
+		return "", errors.New("input is not valid")
+	}
+
+	romanNums := []struct {
+		intValue   int
+		romanValue string
+	}{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
+	result := ""
+	repeat := 0
+
+	for _, v := range romanNums {
+		repeat = input / v.intValue
+		if repeat > 0 {
+			for i := 1; i <= repeat; i++ {
+				result += v.romanValue
+			}
+
+			input = input % v.intValue
+		}
+	}
+
+	return result, nil
 }
