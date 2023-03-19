9b3a61f16f3a2fb8040d88367327c551dd0166fd
barahouei <abdulghanibarahouei@gmail.com>
2023-03-19 22:14:05 +0330

    Solved Collatz Conjecture Exercise

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,25 @@
+// Package collatzconjecture calculate collatz conjecture (3x+1).
 package collatzconjecture
 
+import "errors"
+
+// CollatzConjecture returns the steps from n to 1.
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("zero or negative numbers are not allowed")
+	}
+
+	counter := 0
+
+	for n > 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = n*3 + 1
+		}
+
+		counter++
+	}
+
+	return counter, nil
 }
