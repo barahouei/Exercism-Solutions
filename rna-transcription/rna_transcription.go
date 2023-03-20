720985301342744026d0f3c7c7f283e2a115708c
barahouei <abdulghanibarahouei@gmail.com>
2023-03-20 23:08:20 +0330

    Solved RNA Transcription Exercise

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,27 @@
+// Package strand works with DNA and RNA.
 package strand
 
+// ToRNA takes a DNA strand and returns its RNA correspond.
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	if dna == "" {
+		return ""
+	}
+
+	nucleotides := map[rune]string{
+		'G': "C",
+		'C': "G",
+		'T': "A",
+		'A': "U",
+	}
+
+	var rna string
+
+	for _, v := range dna {
+		nucleotide, ok := nucleotides[v]
+		if ok {
+			rna += nucleotide
+		}
+	}
+
+	return rna
 }
