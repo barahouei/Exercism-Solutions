24d3d01b4b8f1b903ae9741900393f3ffc81ed58
barahouei <abdulghanibarahouei@gmail.com>
2023-03-24 20:11:34 +0330

    Solved Strain Exercise

--- strain/strain.go
+++ strain/strain.go
@@ -1,21 +1,58 @@
+// Package strain checks for predicates on the items.
 package strain
 
 type Ints []int
 type Lists [][]int
 type Strings []string
 
+// Keep returns a new slice of ints where the predicate is true.
 func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+	var list []int
+
+	for _, v := range i {
+		if filter(v) {
+			list = append(list, v)
+		}
+	}
+
+	return list
 }
 
+// Keep returns a new slice of ints where the predicate is false.
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+	var list []int
+
+	for _, v := range i {
+		if !filter(v) {
+			list = append(list, v)
+		}
+	}
+
+	return list
 }
 
+// Keep returns a new slice of [][]ints where the predicate is true.
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+	var list [][]int
+
+	for _, v := range l {
+		if filter(v) {
+			list = append(list, v)
+		}
+	}
+
+	return list
 }
 
+// Keep returns a new slice of strings where the predicate is true.
 func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+	var list []string
+
+	for _, v := range s {
+		if filter(v) {
+			list = append(list, v)
+		}
+	}
+
+	return list
 }
