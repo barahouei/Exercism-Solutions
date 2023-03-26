e8f253fffd900f6fa3f8f1b0b7f63d41c075c505
barahouei <abdulghanibarahouei@gmail.com>
2023-03-26 22:03:30 +0330

    Solved Nth Prime Exercise

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -1,6 +1,41 @@
+// Package prime checks for prime numbers.
 package prime
 
-// Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
+import "errors"
+
+// Nth returns the nth prime number.
 func Nth(n int) (int, error) {
-	panic("Please implement the Nth function")
+	if n < 1 {
+		return 0, errors.New("not valid number")
+	}
+
+	nthPrime := 0
+	counter := 0
+
+	for i := 2; counter <= n; i++ {
+		if isPrime(i) {
+			counter++
+
+			if counter == n {
+				nthPrime = i
+			}
+		}
+	}
+
+	return nthPrime, nil
+}
+
+// isPrime checks if a number is prime.
+func isPrime(n int) bool {
+	if n < 1 {
+		return false
+	}
+
+	for i := 2; i <= n/2; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
