1be892cd871541de0790c6b26650c5d050d82f60
barahouei <abdulghanibarahouei@gmail.com>
2023-04-08 19:15:36 +0330

    Added comment to the etl package

--- etl/etl.go
+++ etl/etl.go
@@ -1,9 +1,12 @@
+// Package etl converts the legacy Scrabble scores format to a new format.
 package etl
 
 import "strings"
 
+// Transform returns a map of each character to its point.
 func Transform(in map[int][]string) map[string]int {
 	m := make(map[string]int)
+
 	for point, list := range in {
 		for _, char := range list {
 			char = strings.ToLower(char)
