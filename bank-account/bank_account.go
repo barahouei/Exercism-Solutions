c6b2b340bc3e8fe141d26677f0a4f6aa011f7e4a
barahouei <abdulghanibarahouei@gmail.com>
2023-04-10 20:39:38 +0330

    Solved Bank Account Exercise

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -1,19 +1,84 @@
+// Package account manages bank account.
 package account
 
-// Define the Account type here.
+import (
+	"sync"
+)
 
+// A customer account.
+type Account struct {
+	mu     sync.Mutex
+	amount int64
+	status bool // status false means account is closed.
+}
+
+// Open returns a new account with the given amount.
 func Open(amount int64) *Account {
-	panic("Please implement the Open function")
+	if amount < 0 {
+		return nil
+	}
+
+	return &Account{amount: amount, status: true}
 }
 
+// Balance returns balance of the account,
+// if account is closed it returns false.
 func (a *Account) Balance() (int64, bool) {
-	panic("Please implement the Balance function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if !a.status {
+		return 0, false
+	}
+
+	return a.amount, true
 }
 
+// Deposit is used for both Deposits and Withdrawals, it returns true if operation succeeds.
+//
+// positive number = Deposit
+//
+// Negative Number = Withdrawal
 func (a *Account) Deposit(amount int64) (int64, bool) {
-	panic("Please implement the Deposit function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if !a.status {
+		return 0, false
+
+	}
+
+	if amount < 0 {
+		amount *= -1 // Convert amount to a positive number.
+
+		if a.amount-amount < 0 {
+			return a.amount, false
+		}
+
+		a.amount -= amount
+
+		return a.amount, true
+	}
+
+	a.amount += amount
+
+	return a.amount, true
 }
 
+// Close closes the account. it returns account's balance,
+// and returns true if account is successfully closed.
 func (a *Account) Close() (int64, bool) {
-	panic("Please implement the Close function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	if !a.status {
+		return 0, false
+	}
+
+	b := a.amount
+
+	a.amount = 0
+	a.status = false
+
+	return b, true
 }
